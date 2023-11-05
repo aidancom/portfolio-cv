8b9444ee40671758150c339ce49df4b460923eaf
aidancom <19aidan99@gmail.com>
2023-11-05 11:24:25 +0100

    Responsive

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,53 @@
 import { Outlet, Link, useLocation } from "react-router-dom";
+import { faInstagram, faFacebook, faLinkedin, faGithub, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../css/Header.scss";
 
 
-const Layout = () => {
+const Layout = ({abierto, setAbierto}) => {
   const location = useLocation();
+  const handleAbierto = () => {
+    if (!abierto) {
+      setAbierto(true);
+      
+    } else {
+      setAbierto(false)
+    }
+
+  }
   return (
     <>
+     <div className="burguer" onClick={handleAbierto} id={abierto ? "equis" : "burguer"}>
+        <span className="burguer-bar"></span>
+        <span className="burguer-bar"></span>
+        <span className="burguer-bar"></span>
+      </div>
+    <div className="cabecera" id={abierto ? "open" : "closed"}>
+     
+     <div className='preheader'>
+        <div className='preheader-social'>
+            <nav>
+              <ul>
+                <li><a href="https://www.instagram.com/a.v.m_99/"><FontAwesomeIcon icon={faInstagram} /></a></li>
+                <li><a href="https://www.facebook.com/aidan.verdejomaynero?locale=es_ES"><FontAwesomeIcon icon={faFacebook} /></a></li>
+                <li><a href="https://www.linkedin.com/in/aidan-verdejo-48496a133/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                <li><a href="https://github.com/aidancom/"><FontAwesomeIcon icon={faGithub} /></a></li>
+              </ul>
+            </nav>
+        </div>
+        <div className='preheader-contacto'>
+          <nav>
+            <ul>
+              <li><a href="mailto:19aidan99@gmail.com"><FontAwesomeIcon icon={faEnvelope} />19aidan99@gmail.com</a></li>
+              <li><a href="tel:+34622881764"><FontAwesomeIcon icon={faPhone} />+34 622 88 17 64</a></li>
+              <li><a href="https://wa.me/+34622881764"><FontAwesomeIcon icon={faWhatsapp} />622 88 17 64</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
       <header>
+     
         <nav>
           <ul>
             <li className={location.pathname === '/' ? 'active' : ''}>
@@ -24,7 +65,9 @@ const Layout = () => {
           </ul>
         </nav>
       </header>
-      <main>
+     
+    </div>
+    <main>
             <Outlet />
       </main>
     </>
