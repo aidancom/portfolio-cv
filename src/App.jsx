f2d6962ee98f80c9480e0172f3c0cb2a7aaa8c05
Aidan <99897929+aidancom@users.noreply.github.com>
2023-11-08 14:24:16 +0000

    Update 9 files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import "./css/App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
-import music from "./assets/music/music.mp3"
 
 import Layout from "./components/Layout";
 import About from "./components/About";
@@ -12,22 +11,23 @@ import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-
 function App() {
   const [abierto, setAbierto] = useState(false);
   const subir = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
-    })
-  }
- 
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Layout abierto={abierto} setAbierto={setAbierto}/>}>
+          <Route
+            path="/"
+            element={<Layout abierto={abierto} setAbierto={setAbierto} />}
+          >
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="proyects" element={<Proyects />} />
@@ -36,9 +36,11 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-     <div className="subir" onClick={subir}>
-      <p><FontAwesomeIcon icon={faAngleUp}></FontAwesomeIcon></p>
-     </div>
+      <div className="subir" onClick={subir}>
+        <p>
+          <FontAwesomeIcon icon={faAngleUp}></FontAwesomeIcon>
+        </p>
+      </div>
     </>
   );
 }
