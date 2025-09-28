12fbeeaf5a43d49dfc653e28f2b5c3a76d6d7819
aidancom <19aidan99@gmail.com>
2025-09-28 10:04:35 +0200

    Se mejora la informacion del visitante

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import datetime
+import requests
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -8,14 +9,32 @@ from connection import column_skills, column_projects, column_visits, columns
 app = Flask(__name__)
 CORS(app)
 
+def get_ip_details(ip):
+  try:
+    url = f"https://ipwho.is/{ip}/"
+    res = requests.get(url)
+
+    if res.status_code != 200:
+      return {}
+    
+    data = res.json()
+    if not data.get("success", True):
+        return {}
+    
+    return data
+  except (requests.exceptions.RequestException, ValueError):
+    return {}
+  
+
 def get_client_ip(request):
-    xff = request.headers.get('X-Forwarded-For', '')
-    if xff:
-        return xff.split(',')[0].strip()
-    cf = request.headers.get('CF-Connecting-IP')
-    if cf:
-        return cf.strip()
-    return request.remote_addr
+  xff = request.headers.get('X-Forwarded-For', '')
+  if xff:
+      return xff.split(',')[0].strip()
+  cf = request.headers.get('CF-Connecting-IP')
+  if cf:
+      return cf.strip()
+  return request.remote_addr
+
 
 @app.route('/api/getData', methods=['POST'])
 def get_data():
@@ -30,22 +49,42 @@ def get_data():
     
   return 200
 
+
 @app.route('/api/sendVisitor')
 def send_visit():
 
   date = datetime.datetime.now()
+  ip = get_client_ip(request)
+  ip_details = get_ip_details(ip) or {}
 
-  data = {
-    'date': f'{date.strftime("%d")}/{date.strftime("%m")}/{date.strftime("%Y")}',
-    'hour': f'{date.strftime("%H")}:{date.strftime("%M")}',
-    'ip': get_client_ip(request)
-  }
+  visitor = column_visits.find_one({"ip": ip})
+  
+  if ip == "127.0.0.1":
+    return jsonify({'status': 'error'})
+  else:
+    if visitor:
+      try:
+        column_visits.update_one({"ip": ip}, {"$inc": {"visits": 1}})
+        return jsonify({'status': 'success'})
+      except Exception as e:
+        return jsonify({'status': 'error', 'message': e})
+    else:
+      try:
+        data = {
+          'ip': ip,
+          'continent': ip_details["continent"],           
+          'country': ip_details["country"],
+          'city': ip_details["city"],      
+          'date': f'{date.strftime("%d")}/{date.strftime("%m")}/{date.strftime("%Y")}',
+          'hour': f'{date.strftime("%H")}:{date.strftime("%M")}',
+          'visits': 1
+        }
 
-  column_visits.insert_one(data)
-
-  return jsonify({'status': 'success'})
+        column_visits.insert_one(data)
+        return jsonify({'status': 'success'})
+      except Exception as e:
+        return jsonify({'status': 'error', 'message': e})
 
   
-
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000)
