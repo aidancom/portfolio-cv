53c68611d7c189d14e4225a33ff45b793123b3c5
aidancom <19aidan99@gmail.com>
2025-09-29 13:19:18 +0200

    Se agrega un backend de backup y se crea un hook para gestionarlo

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -3,36 +3,29 @@ import { useState, useEffect } from "react";
 export const useApi = (endpoint, meth = "GET", body = null) => {
   const [res, setRes] = useState(null);
 
-  const backends = [
-    import.meta.env.VITE_BACK_URL,
-    import.meta.env.VITE_BACK_URL_BACKUP
-  ];
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BACK_URL}/api/${endpoint}`, {
+        method: meth,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: body ? JSON.stringify(body) : null,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setRes(data);
+        return; 
+      } else {
+        console.warn(`Error: ${response.status}`);
+      }
+    } catch (err) {
+      console.warn(`Error con ${backend}, probando siguiente...`);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      for (let backend of backends) {
-        try {
-          const response = await fetch(`${backend}/api/${endpoint}`, {
-            method: meth,
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: body ? JSON.stringify(body) : null,
-          });
-
-          if (response.ok) {
-            const data = await response.json();
-            setRes(data);
-            return; 
-          } else {
-            console.warn(`Error: ${response.status}`);
-          }
-        } catch (err) {
-          console.warn(`Error con ${backend}, probando siguiente...`);
-        }
-      }
-    };
-
     fetchData();
   }, [endpoint, meth, body]);
 
